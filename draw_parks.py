dc2516e392ba25e6f982d7ab88a3196760bc5135
LWynne <58253263+liam-machine@users.noreply.github.com>
2024-10-02 18:59:48 +1000

    code

--- draw_parks.py
+++ draw_parks.py
@@ -11,56 +11,72 @@ temp = img.copy()
 done = False
 points = []
 current = (0, 0)
-prev_current = (0,0)
+prev_current = (0, 0)
+polygon_count = 0
+polygons = []  # List to store all polygons
 
+# Open a file to save the polygon coordinates
+file = open("polygon_coords.txt", "w")
 
 def on_mouse(event, x, y, buttons, user_param):
-    global done, points, current,temp
-    # Mouse callback that gets called for every mouse event (i.e. moving, clicking, etc.)
-    if done: # Nothing more to do
+    global done, points, current, temp
+    # Mouse callback for drawing polygons
+    if done:
         return
     if event == cv2.EVENT_MOUSEMOVE:
-        # We want to be able to draw the line-in-progress, so update current mouse position
         current = (x, y)
     elif event == cv2.EVENT_LBUTTONDOWN:
-        # Left click means adding a point at current position to the list of points
+        # Left click to add points
         print("Adding point #%d with position(%d,%d)" % (len(points), x, y))
-        cv2.circle(img,(x,y),5,(0,200,0),-1)
+        cv2.circle(img, (x, y), 5, (0, 200, 0), -1)
         points.append([x, y])
         temp = img.copy()
     elif event == cv2.EVENT_RBUTTONDOWN:
-        # Right click means we're done
+        # Right click to finish polygon
         print("Completing polygon with %d points." % len(points))
         done = True
 
 cv2.namedWindow("image")
 cv2.setMouseCallback("image", on_mouse)
 
-while(not done):
-            # This is our drawing loop, we just continuously draw new images
-            # and show them in the named window
-            if (len(points) > 1):
-                if(current != prev_current):
-                    img = temp.copy()
-                # Draw all the current polygon segments
-                cv2.polylines(img, [np.array(points)], False, (255,0,0), 1)
-                # And  also show what the current segment would look like
-                cv2.line(img, (points[-1][0],points[-1][1]), current, (0,0,255))
+while True:
+    # Reset the image to the original clone
+    img = clone.copy()
 
-            # Update the window
-            cv2.imshow("image", img)
-            # And wait 50ms before next iteration (this will pump window messages meanwhile)
+    # Draw all the previous polygons
+    for polygon in polygons:
+        cv2.polylines(img, [np.array(polygon)], True, (255, 0, 0), 1)
 
-            if cv2.waitKey(50) == ord('d'): # press d(done)
-                done = True
+    # Draw the current polygon outline if there are points
+    if len(points) > 1:
+        cv2.polylines(img, [np.array(points)], False, (255, 0, 0), 1)
+        cv2.line(img, (points[-1][0], points[-1][1]), current, (0, 0, 255))
 
-# User finised entering the polygon points, so let's make the final drawing
-img = clone.copy()
-# of a filled polygon
-if (len(points) > 0):
-    cv2.fillPoly(img, np.array([points]), (255,0,0))
-# And show it
-cv2.imshow("image", img)
-# Waiting for the user to press any key
-cv2.waitKey(0)
-cv2.destroyWindow("image")
+    # Update the window
+    cv2.imshow("image", img)
+    key = cv2.waitKey(50)
+
+    # Save the polygon when 'd' is pressed
+    if key == ord('d') and len(points) > 2:
+        # Save the polygon coordinates to file
+        file.write(f"Polygon {polygon_count}:\n")
+        for point in points:
+            file.write(f"{point[0]},{point[1]}\n")
+        file.write("\n")
+
+        # Save the polygon to the list of polygons
+        polygons.append(points.copy())
+
+        print(f"Polygon {polygon_count} saved to file and displayed.")
+        polygon_count += 1
+        points = []  # Reset points to allow drawing a new polygon
+        done = False
+
+    # Exit and close the file when 'q' is pressed
+    if key == ord('q'):
+        print("Closing the file and exiting.")
+        break
+
+# Close the file and release resources
+file.close()
+cv2.destroyAllWindows()
